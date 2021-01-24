fcc9fbe30b67165d5a803ee084cd29368960659b
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-24 20:32:44 +0000

    added teleport

---
+++ src/components/GoalCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Goals</h2>
+    <base-card>
+      <ul>
+        <li v-for="goal in goals" :key="goal">{{goal}}</li>
+      </ul>
+    </base-card>
+  </div>
+</template>
+
+<script>
+import BaseCard from "./BaseCard";
+export default {
+  components: {
+    BaseCard
+  },
+  props: {
+    goals: {
+      type: Array
+    }
+  }
+};
+</script>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,8 +3,45 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set goal</button>
+    <teleport to="#app">
+    <error-message v-if="invalidInput">
+      <h2>Invalid input</h2>
+      <p>Please enter input into the box</p>
+      <button @click="errorRecieved">Close</button>
+    </error-message>
+</teleport>
+    <goal-card :goals="goals"></goal-card>
   </div>
 </template>
 
 <script>
+import ErrorMessage from "./ErrorMessage";
+import GoalCard from "./GoalCard";
+
+export default {
+  components: {
+    ErrorMessage,
+    GoalCard
+  },
+  data() {
+    return {
+      invalidInput: false,
+      goals: []
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredInput = this.$refs.goal.value;
+      if (enteredInput === "") {
+        this.invalidInput = true;
+      } else {
+        this.goals.push(enteredInput);
+        this.$refs.goal.value = "";
+      }
+    },
+    errorRecieved() {
+      this.invalidInput = false;
+    }
+  }
+};
 </script>
